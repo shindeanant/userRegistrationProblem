3415a733e198b8cc327a66ec26832536eed94323
shindeanant <shindeanant1997@gmail.com>
2021-10-15 22:29:32 +0530

    Added: Valid first name added

--- main/java/com/bridgelabzz/userregistration/UserRegistration.java
+++ main/java/com/bridgelabzz/userregistration/UserRegistration.java
@@ -4,20 +4,16 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+
+	public void validFirstName() {
 		System.out.println("Enter your First Name : ");
 		String firstName = sc.nextLine();
 		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+	}
 
-		System.out.println("Enter your Last Name : ");
-		String LastName = sc.nextLine();
-		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", LastName));
-
-		System.out.println("Enter your Email ID : ");
-		String email = sc.nextLine();
-		System.out.println(
-				Pattern.matches("\"^([a][b][c])([\\\\.][a-z]+)[@][b][l][\\\\.][c][o]([\\\\.][i][n])\"", email));
-
+	public static void main(String[] args) {
+		UserRegistration object = new UserRegistration();
+		object.validFirstName();
 	}
 }
