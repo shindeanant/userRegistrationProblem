eeb4dd7fbea4218529eefadc8f96eda5bfecd543
shindeanant <shindeanant1997@gmail.com>
2021-10-15 22:12:13 +0530

    Added: Valid last name added

--- main/java/com/bridgelabzz/userregistration/UserRegistration.java
+++ main/java/com/bridgelabzz/userregistration/UserRegistration.java
@@ -6,9 +6,12 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		System.out.println("Enter your First Name : ");
 		String firstName = sc.nextLine();
 		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+
+		System.out.println("Enter your Last Name : ");
+		String LastName = sc.nextLine();
+		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", LastName));
 	}
 }
