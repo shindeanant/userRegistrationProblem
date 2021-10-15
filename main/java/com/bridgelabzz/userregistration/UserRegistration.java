bda0c15579894d3ea24c50f2fff236d5fc31f73a
shindeanant <shindeanant1997@gmail.com>
2021-10-15 22:02:19 +0530

    Added: First name added

---
+++ main/java/com/bridgelabzz/userregistration/UserRegistration.java
@@ -0,0 +1,14 @@
+package com.bridgelabzz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your First Name : ");
+		String firstName = sc.nextLine();
+		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+	}
+}
