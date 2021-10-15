15a30b64ee488aeea3ecbfa86bf6530d735d1f63
shindeanant <shindeanant1997@gmail.com>
2021-10-15 22:18:46 +0530

    Added: Valid email ID added

--- main/java/com/bridgelabzz/userregistration/UserRegistration.java
+++ main/java/com/bridgelabzz/userregistration/UserRegistration.java
@@ -13,5 +13,11 @@ public class UserRegistration {
 		System.out.println("Enter your Last Name : ");
 		String LastName = sc.nextLine();
 		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", LastName));
+
+		System.out.println("Enter your Email ID : ");
+		String email = sc.nextLine();
+		System.out.println(
+				Pattern.matches("\"^([a][b][c])([\\\\.][a-z]+)[@][b][l][\\\\.][c][o]([\\\\.][i][n])\"", email));
+
 	}
 }
